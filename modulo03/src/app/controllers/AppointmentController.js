c6580186029e3bf913ffcc45a145657e2acd9c8f
Celso Alexandre <celsoalexandre@live.com>
2019-12-29 15:24:04 -0300

    Implemented user appointment cancel and simple text mail notification

--- modulo03/src/app/controllers/AppointmentController.js
+++ modulo03/src/app/controllers/AppointmentController.js
@@ -1,11 +1,13 @@
 import * as Yup from 'yup';
-import { startOfHour, isBefore, parseISO, format } from 'date-fns';
+import { startOfHour, isBefore, parseISO, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
 import User from '../models/User';
 import Avatar from '../models/File';
+
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
   async index(req, res) {
@@ -93,6 +95,44 @@ class AppointmentController {
       user_id,
       provider_id,
     });
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment.",
+      });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: 'You can only cancel appointment 2 hours in advance.',
+      });
+    }
+
+    appointment.canceled_at = new Date();
+
+    await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Cancelled Appointment',
+      text: 'An appointment has been cancelled',
+    });
+
+    return res.json(appointment);
   }
 }
 
