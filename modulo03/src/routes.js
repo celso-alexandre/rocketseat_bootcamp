f23b2fa6fd11bdbbad2b26c81a74e9785fd8bf91
Celso Alexandre <celsoalexandre@live.com>
2019-12-29 15:24:04 -0300

    Implemented user appointment cancel and simple text mail nottification

--- modulo03/src/routes.js
+++ modulo03/src/routes.js
@@ -26,10 +26,13 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+// Cancel appointment
+routes.delete('/appointments/:id', AppointmentController.delete);
 
 routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
+// Mark as read
 routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
