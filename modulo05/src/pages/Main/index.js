a1c70acaa04d3fd901f41193f8ac58ae1e7cc43d
Celso Alexandre <celsoalexandre@live.com>
2020-03-29 23:21:50 -0300

    Jumped into Redux Module, and made the initial configuration of reactjs routes, editorconfig, eslint, prettier

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
 import { Container, Form, SubmitButton } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        &nbsp;Repositories
-      </h1>
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+  };
 
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Add Repository" />
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
 
-        <SubmitButton disabled>
-          <FaPlus color="#FFF" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  render() {
+    const { newRepo } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          &nbsp;Repositories
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add Repository"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton disabled>
+            <FaPlus color="#FFF" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
